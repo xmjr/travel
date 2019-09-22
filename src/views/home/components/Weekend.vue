133ac9f9456095271aa2b4522a3838ed7593550d
xmjr <xinlianglai@163.com>
2019-09-22 12:24:26 +0800

    weekend

---
+++ src/views/home/components/Weekend.vue
@@ -0,0 +1,73 @@
+<template>
+	<div>
+		<div class="title">周末去哪儿</div>
+		<ul>
+			<li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+				<div class="item-img-wrapper">
+					<img class="item-img" :src="item.imgUrl" />
+				</div>
+				
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'HomeWeekend',
+		data() {
+			return {
+				recommendList: [{
+					id: '001',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+					title: '京城周末撒欢',
+					desc: '在帝都过周末，不仅仅是城中游！'
+				}, {
+					id: '002',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1811/7e/476589267ebb41.jpg_r_640x214_bf599709.jpg',
+					title: '京城溜娃必去',
+					desc: '德智体美劳全面发展的亲子日，这些地方该去看看…'
+				}, {
+					id: '003',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg',
+					title: '京都必打卡',
+					desc: '来北京必去的景点非这些地方莫属'
+				}, {
+					id: '004',
+					imgUrl: 'http://img1.qunarzz.com/sight/source/1606/85/c6b6bad1b977e.jpg_r_640x214_5c638941.jpg',
+					title: '漂流勇进，烧烤露营',
+					desc: '轻松水上漂，任我乐逍遥'
+				},]
+			}
+		}
+	}
+</script>
+<style lang="stylus" scoped>
+	@import '~@/assets/styles/mixins.styl'
+
+	.title
+		margin-top: .2rem
+		line-height: .8rem
+		background: #eee
+		text-indent: .2rem
+	.item-img-wrapper
+		overflow: hidden
+		height: 0
+		padding-bottom: 33.9%
+	.item-img
+		width: 100%
+	.item-info
+		padding: .2rem
+		.item-title
+			line-height: .54rem
+			font-size: .32rem
+			ellipsis()
+		.item-desc
+			line-height: .4rem
+			color: #ccc
+			ellipsis()
+
+</style>
