6a7a15b1e93696cb1bbc64df526f979c7931a3a7
xmjr <xinlianglai@163.com>
2019-10-31 06:35:10 +0800

    fininsh

---
+++ precache-manifest.8ab2fc65d205f6c01b724f61deca5386.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc5fe83cb7e80fc82d3f",
+    "url": "/travel/css/app.93c49190.css"
+  },
+  {
+    "revision": "dc5fe83cb7e80fc82d3f",
+    "url": "/travel/js/app.8ae9a630.js"
+  },
+  {
+    "revision": "4c8bf92e7a66079a0ea6",
+    "url": "/travel/css/chunk-056ba601.010e6d56.css"
+  },
+  {
+    "revision": "4c8bf92e7a66079a0ea6",
+    "url": "/travel/js/chunk-056ba601.e4ca0c13.js"
+  },
+  {
+    "revision": "2ad0f87fbf29d88149e4",
+    "url": "/travel/css/chunk-176e58ec.5d0d75e4.css"
+  },
+  {
+    "revision": "2ad0f87fbf29d88149e4",
+    "url": "/travel/js/chunk-176e58ec.42031dea.js"
+  },
+  {
+    "revision": "becca21b42d8d323ed2e",
+    "url": "/travel/css/chunk-3345b112.c412edf5.css"
+  },
+  {
+    "revision": "becca21b42d8d323ed2e",
+    "url": "/travel/js/chunk-3345b112.c9ea470d.js"
+  },
+  {
+    "revision": "2176e40a805ed20fa918",
+    "url": "/travel/js/chunk-4ffdd95a.48bb61af.js"
+  },
+  {
+    "revision": "b3e66abf0930d29f9c64",
+    "url": "/travel/css/chunk-cdf38a72.8f1e47ed.css"
+  },
+  {
+    "revision": "b3e66abf0930d29f9c64",
+    "url": "/travel/js/chunk-cdf38a72.13aef1ad.js"
+  },
+  {
+    "revision": "6efd4f9e4af75263826c",
+    "url": "/travel/js/chunk-vendors.121dbf7c.js"
+  },
+  {
+    "revision": "5828168b0bcd3f3684b1660c167b7f58",
+    "url": "/travel/img/iconfont.5828168b.svg"
+  },
+  {
+    "revision": "4f107557cf8fe274ea77d8343135d24d",
+    "url": "/travel/index.html"
+  },
+  {
+    "revision": "2d631f7d51b3f406b3ac764ec3e361f0",
+    "url": "/travel/api/detail.json"
+  },
+  {
+    "revision": "96a7caea90aae8a467d288ccf3cbe402",
+    "url": "/travel/api/index.json"
+  },
+  {
+    "revision": "a86c2684caa29fcf9dcda18791293b6d",
+    "url": "/travel/api/city.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/travel/robots.txt"
+  }
+];
